73743302dfd1890953dfa415f5de9edb74e9387e
Constantine Khrulev <ckhroulev@alaska.edu>
2023-06-27 10:18:28 -0800

    Ignore a bunch of warnings when building icebin

--- scripts/build-icebin.sh
+++ scripts/build-icebin.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 ICEBIN_DIR=~/icebin
-BUILD_DIR=/tmp/icebin-build
+BUILD_DIR=~/icebin-build
 
 mkdir -p ${BUILD_DIR}
 rm -f ${BUILD_DIR}/CMakeCache.txt
 
+# ibmisc headers are full of these...
+IGNORED_WARNINGS="-Wno-ignored-attributes -Wno-narrowing -Wno-vexing-parse -Wno-return-type -Wno-deprecated-declarations"
+
 cmake -S ${ICEBIN_DIR} -B ${BUILD_DIR} \
-  -DCMAKE_BUILD_TYPE=Debug \
-  -DCGAL_DO_NOT_WARN_ABOUT_CMAKE_BUILD_TYPE=TRUE \
-  -DCMAKE_FIND_ROOT_PATH="~/local/blitz;~/local/ibmisc;~/local/spack;~/local/pism" \
-  -DUSE_PISM=TRUE \
+      -DCMAKE_CXX_FLAGS="${IGNORED_WARNINGS}" \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DCGAL_DO_NOT_WARN_ABOUT_CMAKE_BUILD_TYPE=TRUE \
+      -DCMAKE_FIND_ROOT_PATH="~/local/blitz;~/local/ibmisc;~/local/spack;~/local/pism" \
+      -DUSE_PISM=TRUE \
   ;
 
 make -C ${BUILD_DIR} install
