79b36711a8802fd3218e484dc3e8a9d3e64bc756
Constantine Khrulev <ckhroulev@alaska.edu>
2023-06-27 08:59:18 -0800

    Fix build scripts

--- scripts/build-icebin.sh
+++ scripts/build-icebin.sh
@@ -6,11 +6,11 @@ BUILD_DIR=/tmp/icebin-build
 mkdir -p ${BUILD_DIR}
 rm -f ${BUILD_DIR}/CMakeCache.txt
 
-cmake -S ${ICEBIN_DIR} -B ${BUILD_DIR} \\
-  -DCMAKE_BUILD_TYPE=Debug \\
-  -DCGAL_DO_NOT_WARN_ABOUT_CMAKE_BUILD_TYPE=TRUE \\
-  -DCMAKE_FIND_ROOT_PATH="~/local/blitz;~/local/ibmisc;~/local/spack;~/local/pism" \\
-  -DUSE_PISM=TRUE \\
+cmake -S ${ICEBIN_DIR} -B ${BUILD_DIR} \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCGAL_DO_NOT_WARN_ABOUT_CMAKE_BUILD_TYPE=TRUE \
+  -DCMAKE_FIND_ROOT_PATH="~/local/blitz;~/local/ibmisc;~/local/spack;~/local/pism" \
+  -DUSE_PISM=TRUE \
   ;
 
 make -C ${BUILD_DIR} install
--- scripts/build-pism.sh
+++ scripts/build-pism.sh
@@ -2,9 +2,9 @@
 
 PISM_DIR=$HOME/pism
 PREFIX=$HOME/local/pism
-BUILD_DIR=/tmp/pism-build
+BUILD_DIR=$HOME/pism-build
 
-git clone -b mankoff/pism-upgrade https://github.com/NASA-GISS/pism.git ${PISM_DIR} || true
+# git clone -b mankoff/pism-upgrade https://github.com/NASA-GISS/pism.git ${PISM_DIR} || true
 
 mkdir -p ${BUILD_DIR}
 rm -f ${BUILD_DIR}/CMakeCache.txt
@@ -12,9 +12,10 @@ rm -f ${BUILD_DIR}/CMakeCache.txt
 export CC=mpicc
 export CXX=mpicxx
 
-cmake -S ${PISM_DIR} -B ${BUILD_DIR} \\
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \\
-  -DPism_USE_EVERYTRACE=TRUE \\
+cmake -S ${PISM_DIR} -B ${BUILD_DIR} \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DPism_USE_EVERYTRACE=TRUE \
+  -DPism_BUILD_ICEBIN=TRUE \
   ;
 
-make -C ${BUILD_DIR} install
+make -j8 -C ${BUILD_DIR} install
