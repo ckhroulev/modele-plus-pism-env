a4a5a3c8bacfa10fa28f8e10fff861f54b1f64ed
Constantine Khrulev <ckhroulev@alaska.edu>
2023-06-28 15:34:39 -0800

    Working on the ModelE script

--- scripts/build-modele.sh
+++ scripts/build-modele.sh
@@ -18,7 +18,7 @@ make config ModelE_Support=${MODELE_SUPPORT} SHELL=/bin/bash OVERWRITE=YES
 
 echo "COMPILER=gfortran" >> ~/.modelErc
 echo "MPIDISTR=openmpi" >> ~/.modelErc
-echo "MPIDIR=$HOME/local/openmpi" >> ~/.modelErc
+echo "MPIDIR=$HOME/local/spack" >> ~/.modelErc
 
 make rundeck RUN=${RUNNAME} RUNSRC=E4F40
 
@@ -26,6 +26,7 @@ make -j setup \
   RUN=${RUNNAME}  \
   MPI=YES \
   COMPILE_WITH_TRAPS=NO \
+  NETCDFHOME=$HOME/local/spack \
   EXTRA_FFLAGS="-O0 -ggdb3 -fwrapv -fallow-argument-mismatch -fallow-invalid-boz" \
   EXTRA_LFLAGS="-O0 -ggdb3"  \
   2>&1 | tee ${MODELE_TMP}/${RUNNAME}.compile.out
