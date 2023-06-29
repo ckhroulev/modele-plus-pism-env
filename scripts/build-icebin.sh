aee006793b8493d983abced35088589394fe20ef
Constantine Khrulev <ckhroulev@alaska.edu>
2023-06-29 14:29:56 -0800

    Build icebin with modele bits

--- scripts/build-icebin.sh
+++ scripts/build-icebin.sh
@@ -5,6 +5,7 @@ set -x
 set -u
 
 BUILD_DIR=~/local/build/icebin
+PREFIX=$HOME/local/icebin
 
 mkdir -p ${BUILD_DIR}
 rm -f ${BUILD_DIR}/CMakeCache.txt
@@ -13,11 +14,13 @@ rm -f ${BUILD_DIR}/CMakeCache.txt
 IGNORED_WARNINGS="-Wno-ignored-attributes -Wno-narrowing -Wno-vexing-parse -Wno-return-type -Wno-deprecated-declarations -Wno-format"
 
 cmake -S ${ICEBIN_DIR} -B ${BUILD_DIR} \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_CXX_FLAGS="${IGNORED_WARNINGS}" \
       -DCMAKE_BUILD_TYPE=Debug \
       -DCGAL_DO_NOT_WARN_ABOUT_CMAKE_BUILD_TYPE=TRUE \
       -DCMAKE_FIND_ROOT_PATH="~/local/blitz;~/local/ibmisc;~/local/spack;~/local/pism" \
       -DUSE_PISM=TRUE \
+      -DBUILD_MODELE=TRUE \
   ;
 
-make -C ${BUILD_DIR} install
+make -j 8 -C ${BUILD_DIR} install
